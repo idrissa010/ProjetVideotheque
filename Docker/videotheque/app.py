a0cc0931d863a5b4e5611e1a4e4eb2143a13e1d0
Idrissa SOW <isow48130@gmail.com>
2023-12-15 10:06:09 +0000

    Rename folder

--- Docker/videotheque/app.py
+++ Docker/videotheque/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Idrissa!'
+    return 'Hello, Vidéothèque!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
