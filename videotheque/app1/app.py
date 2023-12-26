34d9875614545951aeef12965d4c6d6b96f846c7
MedBakri <bacri.houda@gmail.com>
2023-12-26 19:17:19 +0000

    Add HTML page with JINJA template using JSON Data

--- videotheque/app1/app.py
+++ videotheque/app1/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import json
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/home/toto/ProjetVideotheque/videotheque/templates')
+
+# Get the directory of the script
+json_file_path = os.path.join('data', 'app1_data.json')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Vidéothèque!'
+def index():
+    try:
+        # Load data from the JSON file
+        with open(json_file_path, 'r') as json_file:
+            movie_data = json.load(json_file)
+        return render_template('index.html', movies=movie_data)
+    except FileNotFoundError as e:
+        return f"File not found: {json_file_path}\nError: {e}"
+    except json.JSONDecodeError as e:
+        return f"Error decoding JSON data: {json_file_path}\nError: {e}"
+    except Exception as e:
+        return f"Error loading JSON data: {json_file_path}\nError: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
