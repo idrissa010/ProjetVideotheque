24ff553a1b35c497e7206c0298dbef4a1a5854c0
Idrissa SOW <isow48130@gmail.com>
2024-01-17 17:03:05 +0000

    Implemetation token auth

--- videotheque-api/app2/__init__.py
+++ videotheque-api/app2/__init__.py
@@ -2,11 +2,17 @@
 from flask import Flask
 from app2.controllers.auth_controller import auth_bp
 from app2.controllers.video_controller import video_bp
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('../config.py', silent=True)  # Modifiez le chemin relatif ici
+    app.config.from_pyfile('../config.py', silent=True) 
 
+    # Configurer Flask-JWT-Extended
+    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key' 
+    jwt = JWTManager(app)
+
+    # Enregistrez les blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(video_bp, url_prefix='/video')
 
